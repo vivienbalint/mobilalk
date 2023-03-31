f85550eaf9250260b4c22f2e991ff67af741ada3
Vivien Bálint <h149893@stud.u-szeged.hu>
2023-03-31 15:25:59 +0200

    initial commit

---
+++ app/src/main/java/com/example/vizbnb/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.vizbnb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView nav = findViewById(R.id.navigation);
+        nav.setOnItemSelectedListener(navListener);
+    }
+
+    private final BottomNavigationView.OnItemSelectedListener navListener = item -> {
+        // By using switch we can easily get
+        // the selected fragment
+        // by using there id.
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.profile) {
+            selectedFragment = new ProfileFragment();
+        }
+        // It will help to replace the
+        // one fragment to other.
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        return true;
+    };
+}
